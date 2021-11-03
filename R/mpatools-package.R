dee96d12d339f8e7207c82888c402a28503c9536
Johnathan Evanilla <jevanilla@bigelow.org>
2021-11-03 16:49:35 -0400

    first commit

---
+++ R/mpatools-package.R
@@ -0,0 +1,10 @@
+#' mpatools: Tools for analysis of marine protected areas
+#'
+#'
+#' @docType package
+#' @importFrom dplyr %>%
+#' @importFrom rlang .data !! :=
+#' @importFrom stats complete.cases sd
+#' @name mpatools
+#' @aliases mpatools
+NULL
